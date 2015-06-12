4c363ad6fb175dc4a6c7b575dd98cf785ad3d19e
Jordan Facibene <jordan.facibene13@stjohns.edu>
2015-06-12 18:52:55 -0400

    Update osxTTS.py
    Checks platform once, upon import, instead of every time a function is called.

--- osxTTS.py
+++ osxTTS.py
@@ -4,10 +4,11 @@
 from os import *
 import platform
 
+if platform.system() != 'Darwin': #Check that this is running on OSX
+	raise Exception("You must be running OSX!")
+
 def speak(string,rate): #Outputs speech to the speakers, text is a string, rate is an integer
-	if platform.system()=='Darwin': #Check that this is running on OSX
-		system('say -r '+str(rate)+' "'+string+'"')
+	system('say -r '+str(rate)+' "'+string+'"')
 
 def export(string,rate,filename):#Outputs speech to a file
-	if platform.system()=='Darwin': #Check that this is running on OSX
-		system('say -r '+str(rate)+'-o "'+filename+'" "+string+"')
+	system('say -r '+str(rate)+'-o "'+filename+'" "+string+"')
